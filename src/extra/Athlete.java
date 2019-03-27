f25c3d887bcec84c53ba551affbbaf4723a2b52a
rtgamer8 <league@WTS22.attlocal.net>
2019-03-26 18:28:40 -0700

    took test athlete incomplete

---
+++ src/extra/Athlete.java
@@ -0,0 +1,24 @@
+package extra;
+
+public class Athlete {
+
+	static int nextBibNumber;
+	static String raceLocation = "New York";
+	static String raceStartTime = "9.00am";
+
+	String name;
+	int speed;
+	int bibNumber;
+
+	Athlete(String name, int speed) {
+		this.name = name;
+		this.speed = speed;
+		bibNumber = nextBibNumber;
+		nextBibNumber++;
+	}
+
+public static void main(String[] args) {
+	Athlete 
+	
+}
+}
